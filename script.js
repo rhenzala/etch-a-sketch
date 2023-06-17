2099099def9fd53c615ad724b3bef8993223ad89
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-17 18:59:03 +0800

    Finish the all required functionality

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const DEFAULT_COLOR = '#252424';
 const BOARD_COLOR = '#f1f1f1';
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'color';
+
+let currentMode = DEFAULT_MODE;
 
 const container = document.querySelector('.container');
+const board = document.querySelector('.board');
 const color = document.getElementById('color');
 const rainbow = document.getElementById('rainbow');
 const eraser = document.getElementById('eraser');
@@ -9,31 +14,80 @@ const clear = document.getElementById('clear');
 const sliderValue = document.getElementById('value');
 const slider = document.getElementById('slider');
 
-
-sliderValue.textContent = `${slider.value}x${slider.value}`;
+// could have used the onclick instead of addEventListener
+color.addEventListener("click", () => {
+    setColorMode(DEFAULT_MODE);
+})
+rainbow.addEventListener("click", () => {
+    setColorMode('rainbow');
+})
+eraser.addEventListener("click", () => {
+    setColorMode('eraser');
+})
+clear.addEventListener("click", () => {
+    clearBoard(slider.value);
+})
 slider.addEventListener("mouseup", (e) => {
-    sliderValue.textContent = `${e.target.value}x${e.target.value}`;
+    updateSlideValue(e.target.value)
+})
+slider.addEventListener("change", (e) => {
+    changeSize(e.target.value)
 })
 
-function createBoard(w, h) {
-    const board = document.createElement('div');
-    board.className = "board";
-    for (let i = 0; i < w; i++) {
-      let row = document.createElement('div');
-      row.className = 'row';
-  
-      for (let j = 0; j < h; j++) {
-        let cell = document.createElement('div');
-        cell.className = 'cell';
-  
-        row.appendChild(cell);
+// the rows and columns of the board is defined by the grid property
+function createBoard(size) {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener("mouseenter", changeDivColor)
+        cell.addEventListener("mouseleave", changeDivColor)
+        board.appendChild(cell);
       }
-  
-      board.appendChild(row);
-    }
-  
-    return board;
   }
+// change the color of the cells in the board once the mouse hovers over them
+// e.target applies to cell in the createBoard() function
+function changeDivColor(e){
+    if (e.type === 'mouseenter') return; 
+    if (currentMode === 'rainbow'){
+        rndR = Math.floor(Math.random()*256);
+        rndG = Math.floor(Math.random()*256);
+        rndB = Math.floor(Math.random()*256);
+        e.target.style.backgroundColor = `rgb(${rndR}, ${rndG}, ${rndB})`;
+    }
+    else if (currentMode === 'eraser'){
+        e.target.style.backgroundColor = BOARD_COLOR;
+    }
+    else if (currentMode === 'color'){
+        e.target.style.backgroundColor = DEFAULT_COLOR;
+    }
+}
 
-let board = createBoard(slider.value, slider.value);
-container.appendChild(board);
+function updateSlideValue(value){
+    sliderValue.textContent = `${value}x${value}`;
+}
+// once the user resize the board, it will default the current mode to DEFAULT_MODE
+// which avoids the confusion if the user is currently in eraser mode when he try to draw again
+function changeSize(size){
+    clearBoard(size);
+    updateSlideValue(size);
+    setColorMode(DEFAULT_MODE);
+}
+// clears the board and reset it so that a new session could begin
+function clearBoard(size){
+    board.textContent = '';
+    createBoard(size);
+}
+
+function setColorMode(mode){
+    currentMode = mode;
+}
+
+window.onload = () =>{
+    createBoard(DEFAULT_SIZE);
+    updateSlideValue(slider.value);
+    setColorMode(DEFAULT_MODE);
+}
+
+
